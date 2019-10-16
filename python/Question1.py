35fd88c55d804ddadb0e5c2382f6ebb76afa0086
Harsh Tiwari <47390133+mrgokuji@users.noreply.github.com>
2019-10-16 19:11:06 +0530

    Update Question1.py
    simple interest = p*r*t/100

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,6 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
+    intit
     # modify and then return the variable below
-    answer = -5
+    answer = initialLevelOfDebt*interestPercentage*interestPercentage/100
     return answer
