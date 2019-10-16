ede2486888fe0894879141ab10ae93d45fabdeab
Harsh Tiwari <47390133+mrgokuji@users.noreply.github.com>
2019-10-16 19:16:10 +0530

    Update Question3.py

--- python/Question3.py
+++ python/Question3.py
@@ -1,5 +1,5 @@
 # modify this function, and create other functions below as you wish
 def question03(scores, alice):
     # modify and then return the variable below
-    answer = -5
+    answer = score*alice-50
     return answer
